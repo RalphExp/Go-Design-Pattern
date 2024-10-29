51c384ee84572dc662598c7519dee94821b70e0d
RalphExp <ralphxwz@gmail.com>
2024-10-29 19:41:17 -0400

    update

--- reflect/print.go
+++ reflect/print.go
@@ -1,6 +1,7 @@
 package format
 
 import (
+	"fmt"
 	"reflect"
 	"strconv"
 )
@@ -8,6 +9,20 @@ import (
 // Any formats any value as a string.
 func Any(value interface{}) string {
 	return formatAtom(reflect.ValueOf(value))
+}
+
+func formatArray(v reflect.Value) (s string) {
+	// v is slice or array
+	s += "["
+	if v.Len() > 0 {
+		for i := range v.Len() - 1 {
+			s += formatAtom(v.Index(i))
+			s += " "
+		}
+		s += formatAtom(v.Index(v.Len() - 1))
+	}
+	s += "]"
+	return s
 }
 
 // formatAtom formats a value without inspecting its internal structure.
@@ -33,10 +48,54 @@ func formatAtom(v reflect.Value) string {
 		return strconv.FormatBool(v.Bool())
 	case reflect.String:
 		return strconv.Quote(v.String())
-	case reflect.Chan, reflect.Func, reflect.Ptr, reflect.Slice, reflect.Map:
+	case reflect.Chan, reflect.Func, reflect.Ptr, reflect.Map:
 		return v.Type().String() + " 0x" +
 			strconv.FormatUint(uint64(v.Pointer()), 16)
+	case reflect.Array, reflect.Slice:
+		return formatArray(v)
 	default: // reflect.Array, reflect.Struct, reflect.Interface
 		return v.Type().String() + " value"
 	}
+}
+
+func Display(name string, x interface{}) {
+	fmt.Printf("Display %s (%T):\n", name, x)
+	fmt.Printf(display(name, reflect.ValueOf(x)))
+}
+
+func display(path string, v reflect.Value) (s string) {
+	switch v.Kind() {
+	case reflect.Invalid:
+		s = fmt.Sprintf("%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			s += display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			s += display(fieldPath, v.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			s += display(fmt.Sprintf("%s[%s]", path,
+				formatAtom(key)), v.MapIndex(key))
+		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			s = fmt.Sprintf("%s = nil\n", path)
+		} else {
+			s = display(fmt.Sprintf("(*%s)", path), v.Elem())
+		}
+	case reflect.Interface:
+		if v.IsNil() {
+			s = fmt.Sprintf("%s = nil\n", path)
+		} else {
+			s = fmt.Sprintf("%s.type = %s\n", path, v.Elem().Type())
+			s += display(path+".value", v.Elem())
+		}
+	default: // basic types, channels, funcs
+		s = fmt.Sprintf("%s = %s\n", path, formatAtom(v))
+	}
+	return s
 }
--- reflect/print_test.go
+++ reflect/print_test.go
@@ -1,22 +1,46 @@
 package format
 
 import (
+	"regexp"
 	"testing"
 )
 
 func TestFormat(t *testing.T) {
-	t.Log(Any("hello"))
-	t.Log(Any(3.14159265358979323846264))
-	t.Log(Any(make(chan int)))
-	t.Log(Any(3 + 4i))
-	t.Log(Any(make(map[string]int)))
-	t.Log(Any(true))
-	t.Log(Any(false))
-	t.Log(Any(make([]int, 10)))
-	t.Log(Any(func() {}))
-	t.Log(Any(struct{}{}))
-	t.Log(Any(struct {
+	c := []struct {
+		V interface{}
+		S string
+	}{
+		{3.14159265358979323846264, "3.141592653589793"},
+		{"hello", "^\"hello\"$"},
+		{make(chan int), "^chan int 0x\\w+$"},
+		{make(map[string]int), "map\\[string\\]int 0x\\w+$"},
+		{true, "^true$"},
+		{false, "^false$"},
+		{struct{}{}, "^struct \\{\\} value$"},
+		{struct {
+			A int
+			B int
+		}{}, "^struct \\{ A int; B int \\} value$"},
+		{func() {}, "^func\\(\\) 0x\\w+$"},
+	}
+
+	for _, item := range c {
+		s := Any(item.V)
+		if b, _ := regexp.Match(item.S, []byte(s)); b != true {
+			t.Fatalf("format error: regexp %s does not match %s\n", item.S, s)
+		} else {
+			t.Log(s)
+		}
+	}
+}
+
+func TestDisplay(t *testing.T) {
+	Display("S", struct {
 		A int
 		B int
-	}{}))
+	}{A: 9, B: 22})
+
+	Display("A", [3]int{100, 200, 300})
+	Display("MSI", map[string]int{"A": 1, "B": 2, "C": 3})
+	Display("MAI", map[[3]int]int{{1, 2, 3}: 1, {2, 3, 4}: 2, {4, 5, 6}: 3})
 }
